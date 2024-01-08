daec7a2d920c97cae9f7794afe81a036cc592385
bayukresnaadjie <bayukresnaadjie2@gmail.com>
2024-01-08 12:52:47 +0700

    update

---
+++ home.php
@@ -0,0 +1,11 @@
+<b>
+    <font color="#2133F8"> DASHBOARD</font>
+</b> <br><br>
+<hr color="#2133F8" size="3px" width="100%"><br>
+<font size="+3"><b>Review Singkat Aplikasi</b></font><br><br>
+<font size="+1">
+    <p>Sistem Informasi Universitas XYZ merupakan aplikasi berbasis web. Di aplikasi ini, terdapat pembagian user/multi user yaitu admin,dosen dan mahasiswa.
+        Selain itu aplikasi ini terdiri dari menu Mata Kuliah, Jurusan, Standar Kompetensi, Dosen, Mahasiswa, Wali Murid, dan Nilai. Diharapkan aplikasi ini dapat memudahkan pekerjaan Dosen dalam melakukan pendataan serta penilaian Mahasiswa.</p>
+    <p>Aplikasi ini menggunakan bahasa pemrograman server side (PHP) dan client side (HTML,CSS,dan JAVA SCRIPT), dan menggunakan program XAMPP.</p>
+</font>
+<br>
